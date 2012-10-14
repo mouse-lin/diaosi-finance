be86b5fd5ba6a6b9b0298317dbf0562105c31eca
mouseshi <linhongshimouse@gmail.com>
2012-10-14 23:43:34 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiaosiFinance::Application.config.secret_token = 'b1523a9e1494af5956853261b8d4b78e5ddab04b872c685237e3dcfa5aac895d91ba4f6bf4d5915ae92c683b98db1ff2bb3d3b7278d22331f42a1bea7e74fbbf'
